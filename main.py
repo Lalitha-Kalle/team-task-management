0073b741fa3b9129277842434e992f9dde65bfd2
Lalitha-Kalle <lalithaprogramming@gmail.com>
2026-05-13 17:50:01 +0530

    added tasks list and added get all tasks route

--- main.py
+++ main.py
@@ -2,10 +2,18 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-
+tasks = [
+  "homework",
+  "build frontend",
+  "check api routes"
+]
 
 @app.get("/")
 def home_page():
   return "Home page"
 
 
+
+@app.get("/api/tasks")
+def get_all_tasks():
+  return tasks
